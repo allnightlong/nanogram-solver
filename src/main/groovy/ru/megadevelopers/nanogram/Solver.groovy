a8baa011bf1028ebb40dec5ed4974ecad18c5b15
allnightlong <allnightlong@allnightlong.ru>
2019-06-14 17:37:52 +0300

    source_small

--- src/main/groovy/ru/megadevelopers/nanogram/Solver.groovy
+++ src/main/groovy/ru/megadevelopers/nanogram/Solver.groovy
@@ -2,7 +2,7 @@ package ru.megadevelopers.nanogram
 
 import groovy.json.JsonSlurper
 
-def resource = getClass().getResource('/source.json')
+def resource = getClass().getResource('/source_small.json')
 
 def result = new JsonSlurper().parse(resource)
 result.data_top.each { Collections.replaceAll(it, '', 0) }
@@ -10,4 +10,7 @@ result.data_left.each { Collections.replaceAll(it, '', 0) }
 
 def nanogram = new Nanogram(left: result.data_left, top: result.data_top, height: result.height, width: result.width)
 nanogram.init()
-nanogram.print()
+nanogram.print(false)
+
+nanogram.solve()
+nanogram.print(true)
