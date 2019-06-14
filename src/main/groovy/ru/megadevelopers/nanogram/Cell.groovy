b59aff2bbf1922b7df5545d9862eccd2f26aec0d
allnightlong <allnightlong@allnightlong.ru>
2019-06-14 18:58:05 +0300

    Line refactoring

--- src/main/groovy/ru/megadevelopers/nanogram/Cell.groovy
+++ src/main/groovy/ru/megadevelopers/nanogram/Cell.groovy
@@ -2,7 +2,10 @@ package ru.megadevelopers.nanogram
 
 interface Cell {
 
-    static int NO_VALUE = 0
-    static int EMPTY = 1
-    static int FILLED = 2
+    static int NO_VALUE = -1
+    static int EMPTY = 0
+    static int FILLED = 1
+
+    static char EMPTY_CHAR = '0'
+    static char FILLED_CHAR = '1'
 }
--- src/main/groovy/ru/megadevelopers/nanogram/Line.groovy
+++ src/main/groovy/ru/megadevelopers/nanogram/Line.groovy
@@ -1,46 +1,49 @@
 package ru.megadevelopers.nanogram
 
-
 import groovy.transform.CompileStatic
 
 import static com.google.common.base.Strings.repeat
-import static java.util.Arrays.asList
 
 @CompileStatic
 class Line {
 
     static List<BitSet> candidates(List<Integer> line, int length) {
-        List<BitSet> result = []
 
         int totalFilled = line.sum() as int
 
-        List<String> prep = line.collect { Integer value -> repeat('1', value) }
+        List<String> prepared = line.collect { Integer value -> repeat(Cell.FILLED_CHAR as String, value) }
 
-        for (String r : generateSequence(prep, length - totalFilled + 1)) {
-            char[] bits = r.substring(1).toCharArray()
-            BitSet bitset = new BitSet(bits.length)
-            for (int i = 0; i < bits.length; i++)
-                bitset.set(i, bits[i] == '1')
-            result.add(bitset)
-        }
-        result
+        def sequence = generateSequences(prepared, length - totalFilled + 1)
+
+        sequence.collect { String binary -> fromString(binary.substring(1)) }
     }
 
-    static List<String> generateSequence(List<String> ones, int numZeros) {
+    static List<String> generateSequences(List<String> ones, int numZeros) {
         if (ones.isEmpty()) {
-            return asList(repeat('0', numZeros))
+            return [repeat('0', numZeros)]
         }
 
-        List<String> result = []
+        List<String> sequences = []
         for (int x = 1; x < numZeros - ones.size() + 2; x++) {
-            List<String> skipOne = ones.drop(1)
-            for (String tail : generateSequence(skipOne, numZeros - x)) {
-                result.add(repeat('0', x) + ones.get(0) + tail)
+            String head = ones.first()
+            List<String> tail = ones.drop(1)
+
+            for (String sequence : generateSequences(tail, numZeros - x)) {
+                sequences.add(repeat(Cell.EMPTY_CHAR as String, x) + head + sequence)
             }
         }
-        return result;
+        return sequences
     }
 
+    private static BitSet fromString(String binary) {
+        BitSet set = new BitSet(binary.length())
+        for (int i = 0; i < binary.length(); i++) {
+            if (binary.charAt(i) == Cell.FILLED_CHAR) {
+                set.set(i)
+            }
+        }
+        return set
+    }
 
     protected static List<BitSet> bitSetList(List<List<Integer>> source) {
         source.collect { toBitSet(it) }
